01a12fd081c914ed5b23d0f3182b63021002f33c
Wes Downey <wdowney@rediker.com>
2018-03-12 10:44:12 -0400

    Added Choices to Questions editing within the admin panel

--- poll_app/polls/admin.py
+++ poll_app/polls/admin.py
@@ -3,7 +3,12 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -11,6 +16,7 @@ class QuestionAdmin(admin.ModelAdmin):
         (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
